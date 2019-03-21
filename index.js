203938ea15b9408b1050d747ddda2abe6a1aa83a
laynet <layneingramtaylor@gmail.com>
2019-03-21 12:43:22 -0500

    uuid installed successfully

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 
-const myLibrary = require('./myLibrary');
+const myLibrary = require('./my-library');
+const uuid = require('uuid');
+const newUuid = uuid();
+
+console.log('your new Universally unique identifier is ${newUuid}');
 
 myLibrary.myFirstMethod();
